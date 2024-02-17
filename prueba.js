eecff298a9bad9eb51ed1b37095bcb5608846516
Aranthiël (Natalia Moroni) <nmoronidalmasso@gmail.com>
2024-02-17 18:22:46 -0300

    vista del carrito completa. queda pendiente la confirmacion del pago y los mails de conformacion de compra y recuperacion de contraseña. tambien implementar la recuperacion de contraseña. Tampoco implemente aun passport

--- prueba.js
+++ prueba.js
@@ -1,40 +1,78 @@
-async function updateCartProducts(cartId, products){
-    //obtener los productos actuales del carrito por su id
-   
+async function renderCartDetail(cartId) {
+    console.log('Detalle carrito para renderizar con ID:', cartId);
     try {
-        const carritoDB = await cartsService.getCartById(cartId);
-        
-            
-        
-        
-            
-        // Crear un objeto que mapee cada producto por su ID
-        const currentProductsMap = {};
-        carritoDB.products.forEach(product => {
-            if (product.productId) { // Verificar si el productId está definido
-                currentProductsMap[product.productId] = product.quantity;
-            }
-        });
+        const response = await getCartDetail(cartId);
+        console.log('getCartDetail response', response);
 
-        // Iterar sobre los productos nuevos y actualizar el mapa de productos actuales
-        products.forEach(product => {
-            if (product.productId) { // Verificar si el productId está definido
-                currentProductsMap[product.productId] = product.quantity;
-            }
-        });
-        
+        const tableBody = document.getElementById('cartDetailBody');
+        const title = document.querySelector('h3');
+        title.textContent = `Detalle de carrito ID ${cartId}`;
 
-        // Crear un nuevo array con los objetos de productos y cantidades actualizadas
-        const updatedProducts = Object.keys(currentProductsMap).map(productId => ({
-            productId,
-            quantity: currentProductsMap[productId]
-        }));
-        
-        
-        const updatedCart = await cartsService.updateCart(cartId, updatedProducts);
-        return updatedCart
+        // Limpiar el contenido actual de la tabla
+        tableBody.innerHTML = '';
+
+        // Si la respuesta es exitosa y contiene la información esperada
+        if (response.success && response.cartById && response.cartById.products) {
+            for (const producto of response.cartById.products) {
+                const tr = document.createElement('tr');
+
+                const getProduct = await getProductDetail(producto.productoId);
+                const productDetail = getProduct.productById;
+                
+                if (productDetail) {
+                    const productTitle = document.createElement('td');
+                    productTitle.textContent = productDetail.title;
+                    const productId = document.createElement('span');
+                    productId.textContent = `(${productDetail._id})`;
+                    productId.style.fontSize = '80%';
+                    productTitle.appendChild(productId);
+
+                    const cantidad = document.createElement('td');
+                    cantidad.textContent = producto.quantity;
+
+                    const valorUnitario = document.createElement('td');
+                    valorUnitario.textContent = productDetail.price;
+
+                    const total = document.createElement('td');
+                    total.textContent = producto.quantity * productDetail.price;
+
+                    const addOneBtn = document.createElement('button');
+                    addOneBtn.textContent = '+1';
+                    // Agregar lógica para aumentar la cantidad
+                    // addOneBtn.addEventListener('click', () => {});
+
+                    const removeOneBtn = document.createElement('button');
+                    removeOneBtn.textContent = '-1';
+                    // Agregar lógica para reducir la cantidad
+                    // removeOneBtn.addEventListener('click', () => {});
+
+                    const deleteBtn = document.createElement('button');
+                    deleteBtn.textContent = 'Eliminar';
+                    // Agregar lógica para eliminar el producto del carrito
+                    // deleteBtn.addEventListener('click', () => {});
+
+                    const actions = document.createElement('td');
+                    actions.appendChild(addOneBtn);
+                    actions.appendChild(removeOneBtn);
+                    actions.appendChild(deleteBtn);
+
+                    tr.appendChild(productTitle);
+                    tr.appendChild(cantidad);
+                    tr.appendChild(valorUnitario);
+                    tr.appendChild(total);
+                    tr.appendChild(actions);
+
+                    tableBody.appendChild(tr);
+                } else {
+                    console.error(`No se pudieron obtener detalles del producto con ID ${producto._id}`);
+                }
+            }
+        } else {
+            console.error('La respuesta no contiene la información esperada.');
+        }
     } catch (error) {
-        console.error('Error al actualizar el carrito:', error);
-        throw error;
+        console.error("Error al obtener productos:", error);
     }
 }
+
+renderCartDetail(cid);
--- src/public/js/cartDetail.js
+++ src/public/js/cartDetail.js
@@ -25,7 +25,7 @@ async function getCartDetail(cartId) {
         console.error("Error al obtener productos:", error);
         return [];
     }
-}
+};
 
 async function getProductDetail(productId) {
     try {
@@ -65,7 +65,7 @@ async function renderCartDetail(cartId) {
     
                 const getProduct = await getProductDetail(producto.productoId);
                 const productDetail = getProduct.productById
-                console.log('producto linea 67',productDetail)
+                
                 if (productDetail) {
                     const anchor = document.createElement('a');
                     anchor.href = `${baseURL}/products/${productDetail._id}`;
@@ -82,9 +82,199 @@ async function renderCartDetail(cartId) {
     } catch (error) {
         console.error("Error al obtener productos:", error);
     }
+};
+
+
+
+async function renderCartDetailTabla(cartId) {
+    let totalCompra = 0; // Variable para almacenar el total de la compra
+    try {
+        const response = await getCartDetail(cartId);
+        const tableBody = document.getElementById('cartDetailBody');
+
+        // Limpiar el contenido actual de la tabla
+        tableBody.innerHTML = '';
+
+        // Si la respuesta es exitosa y contiene la información esperada
+        if (response.success && response.cartById && response.cartById.products) {
+            for (const producto of response.cartById.products) {
+                const getProduct = await getProductDetail(producto.productoId);
+                const productDetail = getProduct.productById;
+
+                if (productDetail) {
+                    const row = document.createElement('tr');
+
+                    // Columna de Producto
+                    const productCell = document.createElement('td');
+                    const productLink = document.createElement('a');
+                    productLink.href = `${baseURL}/products/${productDetail._id}`;
+                    productLink.textContent = productDetail.title;
+                    const productIdPara = document.createElement('p');
+                    productIdPara.textContent = productDetail._id;
+                    productCell.appendChild(productLink);
+                    productCell.appendChild(productIdPara);
+                    row.appendChild(productCell);
+
+                    // Columna de Cantidad
+                    const quantityCell = document.createElement('td');
+                    quantityCell.textContent = producto.quantity;
+                    row.appendChild(quantityCell);
+
+                    // Columna de Valor unitario
+                    const priceCell = document.createElement('td');
+                    priceCell.textContent = productDetail.price;
+                    row.appendChild(priceCell);
+
+                    // Columna de Total
+                    const totalProducto = producto.quantity * productDetail.price; // Calculamos el total del producto
+                    totalCompra += totalProducto; // Sumamos al total de la compra
+                    const totalCell = document.createElement('td');
+                    totalCell.textContent = totalProducto;
+                    row.appendChild(totalCell);
+
+                    // Columna de botones de acción
+                    const actionsCell = document.createElement('td');
+                    const btnQuitar = document.createElement('a');
+                    btnQuitar.href = '#';
+                    btnQuitar.textContent = 'Quitar';
+                    btnQuitar.dataset.productId = producto.productoId;
+                    btnQuitar.addEventListener('click', handleRemoveFromCartClick);
+                    actionsCell.appendChild(btnQuitar);
+
+                    const btnAgregar = document.createElement('a');
+                    btnAgregar.href = '#';
+                    btnAgregar.textContent = 'Agregar';
+                    btnAgregar.dataset.productId = producto.productoId;
+                    btnAgregar.addEventListener('click', handleAddToCartClick);
+                    actionsCell.appendChild(btnAgregar);
+
+                    const btnEliminar = document.createElement('a');
+                    btnEliminar.href = '#';
+                    btnEliminar.textContent = 'Eliminar';
+                    btnEliminar.dataset.productId = producto.productoId;
+                    btnEliminar.addEventListener('click', handleDeleteFromCartClick);
+                    actionsCell.appendChild(btnEliminar);
+
+                    row.appendChild(actionsCell);
+
+                    tableBody.appendChild(row);
+                } else {
+                    console.error(`No se pudieron obtener detalles del producto con ID ${producto._id}`);
+                }
+            }
+        } else {
+            console.error('La respuesta no contiene la información esperada.');
+        }
+
+        // Agregar la última fila con el total de la compra
+        const totalRow = document.createElement('tr');
+        const totalTitleCell = document.createElement('td');
+        totalTitleCell.textContent = 'Total de la compra';
+        totalRow.appendChild(totalTitleCell);
+        for (let i = 0; i < 3; i++) { // Llenar tres columnas vacías antes de la columna Total
+            totalRow.appendChild(document.createElement('td'));
+        }
+        const totalCell = document.createElement('td');
+        totalCell.textContent = totalCompra;
+        totalRow.appendChild(totalCell);
+        totalRow.appendChild(document.createElement('td')); // Columna vacía para los botones de acción
+        tableBody.appendChild(totalRow);
+
+    } catch (error) {
+        console.error("Error al obtener productos:", error);
+    }
+};
+
+async function handleAddToCartClick(event) {
+    event.preventDefault();
+    const productId = event.target.dataset.productId;
+    await handleCartAction(productId, 1);
 }
 
-renderCartDetail(cid);
+async function handleRemoveFromCartClick(event) {
+    event.preventDefault();
+    const productId = event.target.dataset.productId;
+    await handleCartAction(productId, -1);
+}
+
+async function handleDeleteFromCartClick(event) {
+    event.preventDefault();
+    Swal.fire({
+        title: 'Función no implementada',
+        text: 'La función para eliminar un producto del carrito aún no está implementada',
+        icon: 'warning',
+        confirmButtonText: 'OK'
+    });
+}
+
+async function handleCartAction(productId, quantity) {
+    let currentUserId;
+    let currentCartId;
+
+    // Obtener información del usuario del Local Storage
+    const userData = JSON.parse(localStorage.getItem('APIuser'));
+    if (userData === null) {
+        currentUserId = "nouser";
+        // Obtener el carrito almacenado en el Local Storage
+        const storedCart = JSON.parse(localStorage.getItem('cart'));
+        if (!storedCart) {
+            currentCartId = null;
+        } else {
+            currentCartId = storedCart.cart;
+        }
+    } else {
+        currentCartId = userData.cart;
+        currentUserId = userData._id;
+    }
+
+    let actionUrl;
+    let method;
+
+    if (currentCartId === null) {
+        actionUrl = `/api/carts/addproduct/${currentUserId}`;
+        method = "POST";
+    } else {
+        actionUrl = `/api/carts/${currentCartId}`;
+        method = "PUT";
+    }
+
+    try {
+        const response = await fetch(actionUrl, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                products: [{
+                    productId: productId,
+                    quantity: quantity
+                }]
+            })
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            const carritoUsuario = responseData.carritoUsuario;
+            console.log('Carrito del usuario:', carritoUsuario);
+
+            localStorage.setItem('cart', JSON.stringify({
+                cart: carritoUsuario._id
+            }));
+            // Actualizar la tabla después de realizar la acción
+            await renderCartDetailTabla(carritoUsuario._id);
+        } else {
+            console.error('Error al realizar la acción en el carrito:', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error('Error en la solicitud fetch:', error);
+    }
+}
+
+
+
+
+renderCartDetailTabla(cid);
+//renderCartDetail(cid);
 
 
 
